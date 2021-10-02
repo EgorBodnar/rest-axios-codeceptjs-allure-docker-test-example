3c89142248b995bce443c4b3aec66c642391c954
EgorBodnar <maxameron@gmail.com>
2021-10-02 18:16:59 +0300

    FEATURE: implement unit-test template

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'node',
+};
+export default config;
---
+++ unit-test/httpService.test.ts
@@ -0,0 +1,9 @@
+describe('unit-test demo', () => {
+  it('should be truthy', () => {
+    const q = true;
+    expect(q).toBeTruthy();
+  });
+  it('should be false', () => {
+    expect(false).not.toBeTruthy();
+  });
+});
