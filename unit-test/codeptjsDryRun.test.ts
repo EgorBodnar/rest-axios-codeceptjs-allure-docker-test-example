d64b007eb4f522e1044995baba7517d6e3b15ba3
EgorBodnar <maxameron@gmail.com>
2021-10-05 20:27:55 +0300

    TEST: http service client unit tests

--- unit-test/codeptjsDryRun.test.ts
+++ unit-test/codeptjsDryRun.test.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 describe('CodeceptJs dry-run check', () => {
-  it('Should not return error code or throw an error', () => {
+  test('Should not return error code or throw an error', () => {
     expect(() => {
       execSync('codeceptjs dry-run', { stdio: 'ignore' });
     }).not.toThrow();
--- unit-test/httpService.test.ts
+++ unit-test/httpService.test.ts
@@ -1,9 +1,61 @@
-describe('unit-test demo', () => {
-  it('should be truthy', () => {
-    const q = true;
-    expect(q).toBeTruthy();
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { mocked } from 'ts-jest/utils';
+import HttpService from '../utils/HttpService';
+
+jest.mock('axios');
+mocked(axios.create).mockImplementation(() => axios);
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const httpService = new HttpService('https://doodle.bom');
+
+describe('HTTP Service client', () => {
+  test('GET should perform the request and receive a list of clients', async () => {
+    const mockedServerResponse: AxiosResponse = {
+      status: 200,
+      statusText: 'SUCCESS',
+      data: [
+        { clientId: 123, name: 'Alex', age: 37 },
+        { clientId: 321, name: 'Ben', age: 52 },
+        { clientId: 432, name: 'Jon', age: 24 },
+      ],
+      config: {},
+      headers: {},
+    };
+    mockedAxios.get.mockResolvedValue(mockedServerResponse);
+    await httpService.get('/clients');
+
+    expect(httpService.response.status).toBe(200);
+    expect(httpService.response.statusText).toBe('SUCCESS');
+    expect(httpService.response.data.length).toBe(3);
+    expect(httpService.response.data).toEqual(mockedServerResponse.data);
   });
-  it('should be false', () => {
-    expect(false).not.toBeTruthy();
+  test('should throw Error if receive error status', async () => {
+    const mockedErrorServerResponse: AxiosError = {
+      code: '505',
+      message: 'ERROR: HTTP Version Not Supported',
+      response: {
+        status: 505,
+        statusText: 'HTTP Version Not Supported',
+        data: {},
+        config: {},
+        headers: {},
+      },
+      config: {},
+      name: '',
+      stack: '',
+      isAxiosError: true,
+      toJSON(): object {
+        return {};
+      },
+    };
+
+    mockedAxios.get.mockRejectedValue(mockedErrorServerResponse);
+
+    await expect(async () =>
+      httpService.get('/error505')
+    ).rejects.toHaveProperty('code', '505');
+    expect(httpService.response).not.toBeUndefined();
+    expect(httpService.response.status).toBe(505);
+    expect(httpService.response.statusText).toBe('HTTP Version Not Supported');
   });
 });
