86a5796b0bcc244b5a3c2be7fcedd52a10ee5752
EgorBodnar <maxameron@gmail.com>
2022-01-31 23:08:23 +0800

    FEATURE: extend data models

---
+++ models/UserCard.ts
@@ -0,0 +1,9 @@
+export class UserCard {
+  id: number;
+
+  first_name: string;
+
+  last_name: string;
+
+  email: string;
+}
---
+++ models/auth/Credentials.ts
@@ -0,0 +1,5 @@
+export class Credentials {
+  username: string;
+
+  password: string;
+}
